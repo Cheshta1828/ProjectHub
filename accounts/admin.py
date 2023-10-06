f4cfded9f7a6fc16755d474f83e67116901d346f
stinger13 <93597591+stinger13@users.noreply.github.com>
2023-10-06 17:52:58 +0530

    login done

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import BaseUser, CollegeSpoc, Course, CourseCoordinator,Visitor
-admin.site.register([BaseUser, CollegeSpoc, Course, CourseCoordinator,Visitor])
+from .models import BaseUser, CollegeSpoc, Course, CourseCoordinator
+admin.site.register([BaseUser, CollegeSpoc, Course, CourseCoordinator])
